81e738b5fcf9608b49601475f373ac2b0f6f5c56
PrimChim <gpritam922@gmail.com>
2024-04-07 16:30:14 +0545

    socials command and gui commands made

--- js/script.js
+++ js/script.js
@@ -13,25 +13,50 @@ const commands = {
     "socials": "check out my social accounts"
 }
 
+document.addEventListener('click', () => {
+    document.getElementById('command').focus()
+})
+
 form.onsubmit = function (e) {
     e.preventDefault();
+    let q = document.createElement('div');
     let input = document.getElementById('command');
+    let output = document.getElementById('terminal-output');
     let value = input.value;
-    if (value === 'help') {
-        let q = document.createElement('p');
-        let container = document.getElementById('terminal-container');
-        q.innerHTML = `<label for="command">
+    if (value === 'gui') {
+        q.innerHTML = `<label >
+        <span class="user">user</span>
+        <span class="neon">@
+        <span class="host">pritamgurung.com.np</span>
+        <span class="neon">: ~$</span>  <span class='user'>gui</span></label>`;
+        output.appendChild(q);
+        window.location.href = 'https://pritamgurung.com.np';
+    }
+
+    if (value === 'socials'){
+        q.innerHTML = `<label >
             <span class="user">user</span>
             <span class="neon">@
-            <span class="host">ethicalhck</span>
+            <span class="host">pritamgurung.com.np</span>
+            <span class="neon">: ~$</span>  <span class='user'>socials</span></label>
+            <div><span class='neon'>My Social Links are:</span></div>
+            <a href='https://github.com/PrimChim'>GitHub</a> <a href='https://linkedin.com/'>LinkedIn</a>`;
+        output.appendChild(q);
+    }
+
+    if (value === 'help') {
+        q.innerHTML = `<label >
+            <span class="user">user</span>
+            <span class="neon">@
+            <span class="host">pritamgurung.com.np</span>
             <span class="neon">: ~$</span>  <span class='user'>help</span></label>`;
 
+        output.appendChild(q);
         for (let key in commands) {
             let div = document.createElement('div');
             div.classList.add('output');
-            div.innerHTML = '<span class="left">'+ key + '</span>'+'<span class="right"> - ' + commands[key] + '</span>';
-            container.appendChild(div);
-            container.appendChild(q);
+            div.innerHTML = '<span class="left">' + key + '</span>' + '<span class="right"> - ' + commands[key] + '</span>';
+            output.appendChild(div);
         }
 
     }
